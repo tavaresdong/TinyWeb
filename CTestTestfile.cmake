08701ac4c1be6b288fc466b8ad36802bbefe664d
tavaresdong <tavares.nju@gmail.com>
2017-01-10 23:15:51 +0800

    Remove make files

--- CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yuchend/working/opnetd
-# Build directory: /home/yuchend/working/opnetd
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(muduo_examples)
--- muduo_examples/echo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yuchend/working/opnetd/muduo_examples/echo
-# Build directory: /home/yuchend/working/opnetd/muduo_examples/echo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
